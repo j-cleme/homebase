e0a281e38abd246266b325286ae6476fa5a2048b
jcleme <clemensjosh@protonmail.com>
2023-04-16 21:14:47 -0400

    start of v1

---
+++ src/lib/IconCard.svelte
@@ -0,0 +1,7 @@
+<script lang="ts"></script>
+
+<div
+	class="border border-black shadow-2xl shadow-black min-w-[25rem] min-h-[25rem] flex justify-center items-center"
+>
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import IconCard from '$lib/IconCard';
+	import { Mail } from 'lucide-svelte';
+</script>
+
+<div class="w-full h-screen bg-stone-900 text-stone-100 flex justify-center items-center">
+	<main class="flex flex-col md:flex-row flex-wrap gap-4 items-baseline">
+		<h1 id="time" class="text-7xl">time</h1>
+		<h1 id="date" class="text-5xl">date</h1>
+		<h1 id="greeting" class="text-3xl">greeting</h1>
+		<h1 id="weather" class="text-4xl">weather</h1>
+
+		<IconCard>
+			<Mail />
+		</IconCard>
+	</main>
+</div>
