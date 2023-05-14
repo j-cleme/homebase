435e9ee8a3201a4f844e3e32e8c9f581d9a7a918
jcleme <clemensjosh@protonmail.com>
2023-05-14 14:12:06 -0400

    started adding functionality

--- src/lib/IconCard.svelte
+++ src/lib/IconCard.svelte
@@ -1,7 +1,11 @@
-<script lang="ts"></script>
+<script lang="ts">
+	export let linkTo: string;
+</script>
 
-<div
-	class="border border-black shadow-2xl shadow-black min-w-[25rem] min-h-[25rem] flex justify-center items-center"
+<a
+	href={linkTo}
+	target="_blank"
+	class="bg-stone-800 p-14 rounded-lg shadow-2xl shadow-black max-w-[25rem] max-h-[25rem] flex justify-center items-center hover:bg-purple-600 hover:-translate-y-2 transform duration-300"
 >
 	<slot />
-</div>
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,40 @@
 <script lang="ts">
-	import IconCard from '$lib/IconCard';
+	import IconCard from '$lib/IconCard.svelte';
 	import { Mail } from 'lucide-svelte';
+	import { onMount } from 'svelte';
+
+	let time = new Date();
+	$: clocktime = time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+	$: date = time.toLocaleString('default', { month: 'long' });
+	$: month = time.getMonth();
+	$: year = time.getFullYear();
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			time = new Date();
+		}, 1000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
 </script>
 
-<div class="w-full h-screen bg-stone-900 text-stone-100 flex justify-center items-center">
+<svelte:head>
+	<title>Homebase</title>
+</svelte:head>
+
+<div class="w-full h-screen bg-stone-900 text-stone-100 font-bold flex justify-center items-center">
 	<main class="flex flex-col md:flex-row flex-wrap gap-4 items-baseline">
-		<h1 id="time" class="text-7xl">time</h1>
-		<h1 id="date" class="text-5xl">date</h1>
+		<h1 id="time" class="text-7xl">{clocktime}</h1>
+		<h1 id="date" class="text-5xl">{date} {month} {year}</h1>
 		<h1 id="greeting" class="text-3xl">greeting</h1>
 		<h1 id="weather" class="text-4xl">weather</h1>
 
-		<IconCard>
-			<Mail />
-		</IconCard>
+		<section id="cards">
+			<IconCard linkTo="https://mail.proton.me">
+				<Mail />
+			</IconCard>
+		</section>
 	</main>
 </div>
