a4ce80b265c7ef1ed678c4482cd6780ad8bba01a
josh clemens <jcleme@proton.me>
2023-09-06 15:36:24 -0400

    added config file for data import, no functional change

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,8 +3,13 @@ export const load = (async ({ fetch }) => {
 
   const api_call = "https://api.weatherbit.io/v2.0/current?&units=I&city=Columbia,MD&key=" + process.env.WEATHER_API
 
-    const res = await fetch(api_call)
+  const res = await fetch(api_call)
   let result = await res.json()
   // console.log(result)
-  return { result }
+  
+  // TODO fetch settings (timezone, buttons, list items, etc.) from json file
+  
+  const config = await fetch("config.json")
+
+  return { result, config }
 }) satisfies PageLoad
